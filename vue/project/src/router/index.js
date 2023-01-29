7a3dd09f4326c24341fef781f1233b906f31572b
chanos-dev <chanos.dev@gmail.com>
2023-01-29 22:23:47 +0900

    vue3+vite todo example
    ref:https://www.youtube.com/watch?v=qhjxAP1hFuI

--- vue/project/src/router/index.js
+++ vue/project/src/router/index.js
@@ -14,6 +14,16 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/databinding/string',
+    name: 'DataBindingStringView',
+    component: () => import(/* webpackChunkName: "databinding" */ '../views/1_databinding/DataBindingStringView.vue')
+  },
+  {
+    path: '/databinding/input',
+    name: 'DataBindingInputView',
+    component: () => import(/* webpackChunkName: "databinding" */ '../views/1_databinding/DataBindingInputView.vue')
   }
 ]
 
